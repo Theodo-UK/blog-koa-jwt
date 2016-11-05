0c0c57c0506700b1c77fa8dbffffa92770f3f410
BrunoGodefroy <brunog@theodo.co.uk>
2016-11-05 13:53:17 +0000

    WIP

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -4,7 +4,7 @@ module.exports = function (ctx) {
   if (JSON.parse(ctx.request.body).password === 'password') {
     ctx.status = 200;
     ctx.body = {
-      token: jwt.sign({ role: 'admin' }, 'A very secret key'), //Should be the same secret key as the one used is ./jwt.js
+      token: jwt.sign({ role: 'admin' }, 'A very secret key'), //Should be the same secret key as the one used in ./jwt.js
       message: "Successfully logged in!"
     };
   } else {
